7cb02ad76e187143455825d28859430d624f1f10
Rokas <rookas.rudzenskas@gmail.com>
2022-09-21 15:02:24 +0300

    ROkas

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,14 +2,19 @@ import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import '../styles/globals.css'
 
+const WalletConnectionProvider = dynamic(() => import('../context/WalletConnectionProvider'), {
+    ssr: false,
+})
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <title>Airbnb App</title>
+                <title>Airbnb Clone</title>
             </Head>
+            <WalletConnectionProvider>
                 <Component {...pageProps} />
+            </WalletConnectionProvider>
         </>
     )
 }
